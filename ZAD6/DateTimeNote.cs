cf985c6b123b052e92e8e441f1b7048910859fde
filip <filip.milkovic1@gmail.com>
2020-05-19 11:01:40 +0200

    Riješen

---
+++ ZAD6/DateTimeNote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD6
+{
+    class DateTimeNote : Note
+    {
+        public DateTime dateTime { get; set; }
+
+        public DateTimeNote() : base() { this.dateTime = DateTime.Now; }
+
+        public DateTimeNote(string text, string author, int importance_level, DateTime dateTime) : base(text, importance_level, author)
+        {
+            this.dateTime = dateTime;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", vrijeme zabilješke: " + this.dateTime;
+        }
+    }
+}
