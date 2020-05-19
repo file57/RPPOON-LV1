84186168d0e017ab3bf0be0496cb24643d31b31d
filip <filip.milkovic1@gmail.com>
2020-05-19 11:00:54 +0200

    Riješen

---
+++ ZAD3/Note.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD3
+{
+    class Note
+    {
+        private String text;
+        private String author;
+        private int importance_level;
+
+        public Note(string text, int importance_level, string author)
+        {
+            this.text = text;
+            this.importance_level = importance_level;
+            this.author = author;
+        }
+        public Note()
+        {
+            this.text = "Tekst";
+            this.importance_level = 5;
+            this.author = "Autor";
+        }
+        public Note(string text, string author)
+        {
+            this.text = text;
+            this.importance_level = 2;
+            this.author = author;
+        }
+
+        private void setAuthor(String author)
+        {
+            this.author = author;
+        }
+        public void setText(String text)
+        {
+            this.text = text;
+        }
+        public void setImportance_level(int importance_level)
+        {
+            this.importance_level = importance_level;
+        }
+        public String getAuthor()
+        {
+            return author;
+        }
+        public String getText()
+        {
+            return text;
+        }
+        public int getImportance_level()
+        {
+            return importance_level;
+        }
+    }
+}
---
+++ ZAD3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Note zabiljeska1 = new Note();
+            Note zabiljeska2 = new Note("ovaj text", "Veliki autor");
+            Note zabiljeska3 = new Note("Jos jedan text", 5, "Mali autor");
+
+            Console.WriteLine("autor zabiljeske1 je {0}, a tekst joj je {1};\n", zabiljeska1.getAuthor(), zabiljeska1.getText());
+            Console.WriteLine("autor zabiljeske2 je {0}, a tekst joj je {1};\n", zabiljeska2.getAuthor(), zabiljeska2.getText());
+            Console.WriteLine("autor zabiljeske3 je {0}, a tekst joj je {1};\n", zabiljeska3.getAuthor(), zabiljeska3.getText());
+
+        }
+    }
+}
